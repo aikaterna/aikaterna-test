7fa17b8366006fc2c728b8224578d78611f38bf3
aikaterna <aikaterna@hotmail.com>
2016-11-18 00:45:09 -0800

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class Test:
+	"""Test cog to push commits for testing the downloader."""
+	def __init__(self,bot):
+		self.bot = bot
+
+	@commands.command(pass_context=True)
+	async def test(self, ctx):
+		"""I really hope you didn't install this."""
+		await self.bot.say("\N{SPARKLES}")
+
+def setup(bot):
+	n = Test(bot)
+	bot.add_cog(n)
